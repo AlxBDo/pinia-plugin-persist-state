200ab844fa5ed8d35a6a3b7b710b758b32276950
AlxBDo <alexbidaud85@gmail.com>
2026-08-17 22:42:36 +0200

    chore: repo security improvment

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,15 @@
+import type { PluginSubscriberInterface } from "pinia-plugin-subscription";
+
 export type {
     PersistedStore,
     PersistedStoreOptions,
     PluginPersistedStoreOptions,
 } from "./store";
+
+export declare const PLUGIN_NAME: string
+
+export declare const PeristPiniaState: PluginSubscriberInterface
+
+export declare function createPersistStatePlugin(dbName?: string, cryptKey?: string): PluginSubscriberInterface
+
+export declare function createPluginMock(dbName: string, cryptKey?: string): PluginSubscriberInterface
